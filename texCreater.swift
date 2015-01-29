05553685ebd6408266c627ee1aa4c5fc1b8eccb0
= <atto.mark56@gmail.com>
2015-01-29 18:55:21 +0900

    replace texCreater.swift

--- texCreater.swift
+++ texCreater.swift
@@ -1,4 +1,36 @@
 #!/usr/bin/swift
+import Foundation
+  
+var texargs = NSString()
+var dviargs = NSString()
+
+
+for arg in Process.arguments {
+  texargs =  "platex"+" \(arg)"+".tex"
+  dviargs =  "dvipdfmx"+" \(arg)"+".dvi"
+}
+
+var texTask = NSTask()
+texTask.launchPath = "/bin/sh"
+texTask.arguments = ["-c", texargs]
+texTask.standardInput = NSFileHandle.fileHandleWithNullDevice()
+texTask.standardError = NSFileHandle.fileHandleWithStandardError()
+texTask.standardOutput = NSFileHandle.fileHandleWithStandardOutput()
+texTask.launch()
+texTask.waitUntilExit()
+
+
+
+
+let dviTask = NSTask()
+dviTask.launchPath = "/bin/sh"
+dviTask.arguments = ["-c", dviargs]
+//task.arguments = Process.arguments
+dviTask.standardInput = NSFileHandle.fileHandleWithNullDevice()
+dviTask.standardError = NSFileHandle.fileHandleWithStandardError()
+dviTask.standardOutput = NSFileHandle.fileHandleWithStandardOutput()
+dviTask.launch()
+dviTask.waitUntilExit()#!/usr/bin/swift
 
 import Foundation
 
